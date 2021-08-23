eba981736def5afd05cadbf478a6890977cc3b9f
jacquelineb <jacquelinesotheremail@gmail.com>
2021-08-23 11:32:04 -0700

    Styled navbar and added to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import NavBar from './components/NavBar';
 import style from './styles/App.module.scss';
 
 function App() {
   return (
-    <div className={style.mainContent}>
+    <>
       <Router>
-        <Switch>
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-        </Switch>
+        <NavBar />
+        <div className={style.mainContent}>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
+          </Switch>
+        </div>
       </Router>
-    </div>
+    </>
   );
 }
 
